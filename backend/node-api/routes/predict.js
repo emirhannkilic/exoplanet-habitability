ce9c01bb02785ac47275d30f710aa64534929000
Emirhan Kılıç <emirrk53@gmail.com>
2026-02-17 01:53:27 +0300

    Remove backend folder (ML-only project)

--- backend/node-api/routes/predict.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { predict } = require('../controllers/predictController');
-
-router.post('/', predict);
-
-module.exports = router;
